d582977d1ce0a9f974155ef35ec9b43398cdd191
weroh <50031971+weroh@users.noreply.github.com>
2019-09-24 15:01:11 -0700

    Tests for API Endpoints
    - Tests for API Endpoints
    - Also removed an unnecessary line from Telnyx.php

---
+++ tests/api_resources/MessagingProfileTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Telnyx;
+
+
+class MessagingProfileTest extends TestCase
+{
+    const TEST_RESOURCE_ID = '123';
+
+
+    public function testIsListable()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v2/messaging_profiles'
+        );
+        $resources = MessagingProfile::all();
+        $this->assertInstanceOf(\Telnyx\Collection::class, $resources);
+        $this->assertInstanceOf(\Telnyx\MessagingProfile::class, $resources[0]);
+    }
+
+    public function testIsRetrievable()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v2/messaging_profiles/' . urlencode(self::TEST_RESOURCE_ID)
+        );
+        $resource = MessagingProfile::retrieve(self::TEST_RESOURCE_ID);
+        $this->assertInstanceOf(\Telnyx\MessagingProfile::class, $resource);
+    }
+
+
+    public function testIsCreatable()
+    {
+        $this->expectsRequest(
+            'post',
+            '/v2/messaging_profiles'
+        );
+        $resource = MessagingProfile::create(["country" => "US", "type" => "custom"]);
+        $this->assertInstanceOf(\Telnyx\MessagingProfile::class, $resource);
+    }
+
+    public function testIsUpdatable()
+    {
+        $this->expectsRequest(
+            'patch',
+            '/v2/messaging_profiles/' . urlencode(self::TEST_RESOURCE_ID)
+        );
+        $resource = MessagingProfile::update(self::TEST_RESOURCE_ID, [
+            "name" => "Test",
+        ]);
+        $this->assertInstanceOf(\Telnyx\MessagingProfile::class, $resource);
+    }
+
+    public function testIsDeletable()
+    {
+        $resource = MessagingProfile::retrieve(self::TEST_RESOURCE_ID);
+        $this->expectsRequest(
+            'delete',
+            '/v2/messaging_profiles/' . urlencode(self::TEST_RESOURCE_ID)
+        );
+        $resource->delete();
+        $this->assertInstanceOf(\Telnyx\MessagingProfile::class, $resource);
+    }
+
+    public function testCanCallPhoneNumbers()
+    {
+        $messaging_profile = MessagingProfile::retrieve(self::TEST_RESOURCE_ID);
+        $this->expectsRequest(
+            'get',
+            '/v2/messaging_profiles/' . urlencode(self::TEST_RESOURCE_ID) . '/phone_numbers'
+        );
+        $resources = $messaging_profile->phone_numbers();
+        $this->assertInstanceOf(\Telnyx\MessagingProfile::class, $resources);
+        $this->assertInstanceOf(\Telnyx\MessagingPhoneNumber::class, $resources[0]);
+    }
+
+    public function testCanCallShortCodes()
+    {
+        $messaging_profile = MessagingProfile::retrieve(self::TEST_RESOURCE_ID);
+        $this->expectsRequest(
+            'get',
+            '/v2/messaging_profiles/' . urlencode(self::TEST_RESOURCE_ID) . '/short_codes'
+        );
+        $resources = $messaging_profile->short_codes();
+        $this->assertInstanceOf(\Telnyx\MessagingProfile::class, $resources);
+        $this->assertInstanceOf(\Telnyx\MessagingShortCode::class, $resources[0]);
+    }
+
+
+    public function testCanCallSenderIds()
+    {
+        $messaging_profile = MessagingProfile::retrieve(self::TEST_RESOURCE_ID);
+        $this->expectsRequest(
+            'get',
+            '/v2/messaging_profiles/' . urlencode(self::TEST_RESOURCE_ID) . '/sender_ids'
+        );
+        $resources = $messaging_profile->sender_ids();
+        $this->assertInstanceOf(\Telnyx\MessagingProfile::class, $resources);
+        $this->assertInstanceOf(\Telnyx\MessagingSenderId::class, $resources[0]);
+    }
+
+}
---
+++ tests/api_resources/NumberReservationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Telnyx;
+
+class NumberReservationTest extends TestCase
+{
+    const NUMBER_RESERVATION_ID = "f7964e2b-a9f9-4eb6-ab16-e570ffc4bc83";
+
+    public function testIsListable()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v2/number_reservations'
+        );
+        $resources = NumberReservation::all();
+        $this->assertInstanceOf(\Telnyx\Collection::class, $resources);
+        $this->assertInstanceOf(\Telnyx\NumberReservation::class, $resources[0]);
+    }
+
+    public function testIsRetrievable()
+    {
+        $this->expectsRequest(
+            'get',
+            '/v2/number_reservations/' . urlencode(self::NUMBER_RESERVATION_ID)
+        );
+        $resource = NumberReservation::retrieve(self::NUMBER_RESERVATION_ID);
+        $this->assertInstanceOf(\Telnyx\NumberReservation::class, $resource);
+    }
+
+}
