54c9de82904a90edaf5a4b1bb03d4024531f67de
weroh <50031971+weroh@users.noreply.github.com>
2020-06-02 13:20:21 -0700

    Hotfix for Bug 32 (#34)
    Fixed bug with title:
    "meta" is being looked for incorrectly instead of "metadata" in API response

--- lib/Util/Util.php
+++ lib/Util/Util.php
@@ -117,7 +117,7 @@ abstract class Util
         } elseif (is_array($resp)) {
             if (isset($resp['record_type']) && is_string($resp['record_type']) && isset($types[$resp['record_type']])) {
                 $class = $types[$resp['record_type']];
-            } elseif (isset($resp['meta']) || isset($resp['data'][0])) { // Only Collections will have 'meta' and this is how we detect collections
+            } elseif (isset($resp['meta']) || isset($resp['metadata']) || isset($resp['data'][0])) { // Only Collections will have 'meta' and this is how we detect collections
                 $class = 'Telnyx\\Collection';
             } else {
                 $class = 'Telnyx\\TelnyxObject';
