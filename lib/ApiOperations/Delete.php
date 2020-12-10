b2ef6175d635993dc9403bd7853b647d95e146da
Kevin Korb <kevin@echo19.com>
2020-12-10 10:20:34 -0600

    Modified the delete method tobe able to be called statically to match documentation, as well as other methods.

--- lib/ApiOperations/Delete.php
+++ lib/ApiOperations/Delete.php
@@ -9,19 +9,23 @@ namespace Telnyx\ApiOperations;
  */
 trait Delete
 {
-    /**
-     * @param array|null $params
-     * @param array|string|null $opts
-     *
-     * @return \Telnyx\ApiResource The deleted resource.
-     */
-    public function delete($params = null, $opts = null)
-    {
-        self::_validateParams($params);
+	/**
+	 * @param string $id The ID of the resource to delete.
+	 * @param array|null $params
+	 * @param array|string|null $opts
+	 *
+	 * @return \Telnyx\ApiResource The updated resource.
+	 */
+	public static function delete($id, $params = null, $opts = null)
+	{
+		self::_validateParams($params);
 
-        $url = $this->instanceUrl();
-        list($response, $opts) = $this->_request('delete', $url, $params, $opts);
-        $this->refreshFrom($response, $opts);
-        return $this;
-    }
+		$url = static::resourceUrl($id);
+
+		list($response, $opts) = static::_staticRequest('delete', $url, $params, $opts);
+		$obj = \Telnyx\Util\Util::convertToTelnyxObject($response->json, $opts);
+		$obj->setLastResponse($response);
+
+		return $obj;
+	}
 }
