f0cb2bba2a1bd943af1eb40d613710463b61f8ee
weroh <50031971+weroh@users.noreply.github.com>
2021-08-12 23:54:18 -0700

    Revert PortingPhoneNumber.php

--- lib/PortingPhoneNumber.php
+++ lib/PortingPhoneNumber.php
@@ -9,18 +9,7 @@ namespace Telnyx;
  */
 class PortingPhoneNumber extends ApiResource
 {
-    const OBJECT_NAME = "number_order_phone_number";
+    const OBJECT_NAME = "porting_phone_number";
 
     use ApiOperations\All;
-
-    /**
-     * @return string The endpoint URL for the given class.
-     */
-    public static function classUrl()
-    {
-        // NOTE: This function override compensates for the different way this endpoint is spelled.
-        // 'faxs' vs 'faxes'
-        // Original function inside ApiResource.php
-        return "/v2/porting_phone_numbers";
-    }
 }
