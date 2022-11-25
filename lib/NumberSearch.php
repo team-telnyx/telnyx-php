1b298a2c7ac8d36ebcbad4802bcd051824693384
Onur Özgür ÖZKAN <oozgurozkan@gmail.com>
2022-11-25 03:58:23 +0000

    Add NumberSearch endpoint.
    Add https://developers.telnyx.com/docs/api/v2/numbers/Number-Search endpoint.
    
    ```
    \Telnyx\Telnyx::setApiKey('Token');
    
    // Get 10 available  numbers
    $result = \Telnyx\NumberSearch::all();
    
    // Or
    
    // Get 10 available numbers from GB.
    $args = [ 'filter' => ['country_code' => 'GB']];
    $result = \Telnyx\NumberSearch::all($args);
    
    // Output list of messaging profiles
    print_r($result);
    
    // Output list of messaging profiles
    print_r($result);
    ```

---
+++ lib/NumberSearch.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Telnyx;
+
+/**
+ * Class NumberSearch
+ *
+ * @package Telnyx
+ */
+class NumberSearch extends ApiResource
+{
+    const OBJECT_NAME = "available_phone_number";
+
+    use ApiOperations\All;
+}
