1eb26ceda556df0d7646c640da4d858c21875177
vionash <vladi@telnyx.com>
2023-09-13 00:32:03 -0500

    Added 10DLC Endpoints and version bump

---
+++ lib/PhoneNumberAssignmentByProfile.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Telnyx;
+
+/**
+ * Class Address
+ *
+ * @package Telnyx
+ */
+class Address extends ApiResource
+{
+    const OBJECT_NAME = "phone_number_assignment_by_profile";
+
+    use ApiOperations\All;
+    use ApiOperations\Create;
+    use ApiOperations\Retrieve;
+    
+    /**
+     * @return string The endpoint associated with this singleton class.
+     */
+    public static function classUrl()
+    {
+        // Use a custom URL for this resource
+        // NOTE: This endpoint is special because object name and endpoint are both not plural 
+        return "/v2/phoneNumberAssignmnetByProfile";
+    }
+    public function phone_numbers($params = null, $options = null)
+    {
+        $url = $this->instanceUrl() . '/phoneNumbers';
+        list($response, $opts) = $this->_request('get', $url, $params, $options);
+        $this->refreshFrom($response, $opts);
+        return $this;
+    }
+}
